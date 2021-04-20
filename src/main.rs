6786f2e375c8ac1ba1ffeda91925d29b522c981a
Cyrill Leutwiler <bigcyrill@hotmail.com>
2021-04-20 22:27:10 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use bevy::prelude::*;
+
+struct Person;
+struct Name(String);
+
+pub struct HelloPlugin;
+struct GreetTimer(Timer);
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if !timer.0.tick(time.delta()).just_finished() {
+        return;
+    }
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
